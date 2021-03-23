b0265c6acac0bcd7a9899c342b9268e200f17100
Veda Vyas Dyawanapally <vedavyas.vvs32@gmail.com>
2021-03-22 22:54:01 -0400

    Cart Managemnt project added

---
+++ Cart Management/cart.js
@@ -0,0 +1,37 @@
+var empObj = [];
+function storeInSession() {
+    sessionStorage.setItem("projectinfo", JSON.stringify(empObj));
+}
+function retrieveFromSession() {
+    var obj = JSON.parse(sessionStorage.getItem("projectinfo"));
+    console.log(obj);
+}
+function onFormSubmit() {
+    //alert("Event generated...")
+    var data = readFormData();
+    insertNewRecord(data);
+    empObj.push(data); //in empObj
+    resetData();
+    console.log('item');
+    console.log('price');
+}
+function readFormData() {
+    var obj = {}; // empty object
+    var obj1 = document.getElementById('item');
+    var obj2 = document.getElementById('price');
+    console.log(obj);
+    return obj;
+}
+function insertNewRecord(data) {
+    var table = document.getElementById("details");
+    var body = table.getElementsByTagName("tbody")[0];
+    var newRow = body.insertRow(); // row created
+    var cell1 = newRow.insertCell(0); // cell created
+    cell1.innerHTML = data.item; // value placed
+    var cell2 = newRow.insertCell(1); // cell created
+    cell2.innerHTML = data.price; // value placed
+}
+function resetData() {
+    console.log(document.getElementById('item'));
+    console.log(document.getElementById('price'));
+}
