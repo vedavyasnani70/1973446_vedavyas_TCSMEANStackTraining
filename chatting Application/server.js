94d3914d610558fa0d09ca2f137f8ecebbeb3a13
Veda Vyas Dyawanapally <vedavyas.vvs32@gmail.com>
2021-04-13 22:06:04 -0400

    chatting Application

---
+++ chatting Application/server.js
@@ -0,0 +1,22 @@
+let app = require("express")();
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+io.on("connection",(socket)=>{
+    console.log("Client connected to application....")
+
+    socket.on("chat message",(msg)=> {
+        console.log(msg);
+    })
+})
+
+
+
+
+http.listen(9090,()=>console.log('server running on port number 9090'));
+
+
